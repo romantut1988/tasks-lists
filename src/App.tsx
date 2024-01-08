1f9ca00655d53a027e67c665fd23abd2248286ea
Roman <romantut1988@mail.ru>
2024-01-08 09:39:02 +0300

    add uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from './Todolist';
+import {v1} from "uuid";
 
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
     let [tasks, setTasks] = useState<Array<TaskType>>([
-            {id: 1, title: "HTML&CSS", isDone: true},
-            {id: 2, title: "JS", isDone: true},
-            {id: 3, title: "ReactJS", isDone: false},
-            {id: 4, title: "Redux", isDone: false}
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Redux", isDone: false}
         ]
     )
 
     let [filter, setFilter] = useState<FilterValuesType>("all");
 
-    function removeTask(id: number) {
+    function removeTask(id: string) {
         let filteredTasks = tasks.filter(t => t.id !== id)
         setTasks(filteredTasks)
     }
